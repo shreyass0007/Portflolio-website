aa7999689d30518bfc66ab53af1429978aeb34cb
shreyass0007 <shreshshende.777@gmail.com>
2025-04-03 12:17:22 +0530

    Fix build issues and add missing dependencies

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  [key: string]: any
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
